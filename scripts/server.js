eb78132cde86d9eefc4e6926f2a5c987aea1f7ad
Nir Hirschel <133850819+Rdhirschel@users.noreply.github.com>
2023-12-07 09:16:49 +0000

    Update URLs in HTML files

--- scripts/server.js
+++ scripts/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const WebSocket = require('ws');
 
 const app = express();
-const port = 3000;
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -19,7 +24,8 @@ app.post('/createFile', (req, res) => {
         return res.status(400).json({ error: 'Missing filePath or content in the request body' });
     }
 
-    const fullPath = path.resolve(process.cwd(), filePath);
+    const fullPath = path.resolve(__dirname, 'public', filePath);
+    console.log('Full Path:', fullPath);
 
     try {
         fs.writeFileSync(fullPath, content);
@@ -28,11 +34,28 @@ app.post('/createFile', (req, res) => {
         console.error(error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
+
+    // Notify connected clients about the file creation
+    wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify({ event: 'fileCreated', filePath }));
+        }
+    });
 });
 
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
 
-app.listen(port, () => {
+// WebSocket connection handling
+wss.on('connection', (ws) => {
+    console.log('WebSocket connection established');
+
+    // Handle messages from clients if needed
+    ws.on('message', (message) => {
+        console.log('Received message:', message);
+    });
+});
+
+server.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
--- scripts/url.js
+++ scripts/url.js
@@ -1,6 +1,23 @@
-const folderPath = 'short';
-const localStorageKey = 'saves';
-const serverUrl = window.location.origin + '/createFile';
+folderrPath = './short';
+const localStorageKey = './short/saves.json';
+const serverUrl = `ws://${window.location.host}`;
+const socket = new WebSocket(serverUrl);
+
+socket.addEventListener('open', (event) => {
+    console.log('WebSocket connection opened:', event);
+});
+
+socket.addEventListener('message', (event) => {
+    const data = JSON.parse(event.data);
+
+    if (data.event === 'fileCreated') {
+        console.log(`File created: ${data.filePath}`);
+    }
+});
+
+socket.addEventListener('close', (event) => {
+    console.log('WebSocket connection closed:', event);
+});
 
 function updateLocalStorage(linkName, url) 
 {
@@ -33,10 +50,11 @@ async function createHtmlPage(url, linkName)
     `;
 
     // Send a request to the server to create the HTML file
-    await fetch(serverUrl, {
+    await fetch("./short", {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
+            'Authorization': 'Bearer ${process.env.ACCESS_KEY}',
         },
         body: JSON.stringify({
             filePath: `${folderPath}/${sanitizedLinkName}.html`,
@@ -47,10 +65,14 @@ async function createHtmlPage(url, linkName)
 
 function urlExists(url) 
 {
-    try {
+    try 
+    {
         new URL(url);
         return true;
-    } catch (error) {
+    } 
+    
+    catch (error) 
+    {
         return false;
     }
 }
@@ -59,10 +81,13 @@ async function shortenURL() {
     const url = document.getElementById('url-input').value.trim();
     const linkName = document.getElementById('name-input').value.trim();
 
-    if (url.length === 0) {
+    if (url.length === 0) 
+    {
         document.getElementById('result').innerText = '';
         return;
-    } else if (!urlExists(url)) {
+    } 
+    else if (!urlExists(url)) 
+    {
         document.getElementById('result').innerText = 'Please enter a valid URL';
         return;
     }
@@ -70,7 +95,8 @@ async function shortenURL() {
     const storedData = localStorage.getItem(localStorageKey);
     const data = storedData ? JSON.parse(storedData) : {};
 
-    if (data[linkName]) {
+    if (data[linkName] == url) 
+    {
         document.getElementById('result').innerText = `Link already exists. HTML Page: ${getHtmlPageLink(linkName)}`;
         return;
     }
@@ -80,7 +106,7 @@ async function shortenURL() {
     document.getElementById('result').innerHTML = `Link shortened successfully. HTML Page: ${getHtmlPageLink(linkName)}`; 
 }
 
-function getHtmlPageLink(linkName) {
-    // Create a link to view the HTML page on the server
-    return `<a href="${serverUrl}/${folderPath}/${linkName}.html" target="_blank">${folderPath}/${linkName}.html</a>`;
+function getHtmlPageLink(linkName) 
+{
+    return `<a href="${window.location.origin}/short/${linkName}.html" target="_blank">short/${linkName}.html</a>`;
 }
