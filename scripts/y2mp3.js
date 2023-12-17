583f7804164b58a0d22d72abb37fb46e1799c84b
creatorOfManyWonders <153096083+creatorOfManyWonders@users.noreply.github.com>
2023-12-17 09:27:34 +0000

    i cry

--- scripts/y2mp3.js
+++ scripts/y2mp3.js
@@ -1,18 +1,23 @@
+//const ytdl = require('ytdl-core');
+
+
 async function convertYoutubeToMp3(url, outputFilename) {
+   document.getElementById("error").textContent = "asdjfsasdf";
+
    try {
-      require(['ytdl-core', 'fluent-ffmpeg', 'fs'], function (ytdl, ffmpeg, fs) {
+         
          const info = ytdl.getInfo(url);
          // Select the video format and quality
          const format = ytdl.chooseFormat(info.formats, { quality: "248" });
          // Create a write stream to save the video file
-         const outputFilePath = `${info.videoDetails.title}.${format.container}`;
+         const outputFilePath = 'audio/' + `${info.videoDetails.title}.${format.container}`;
          const outputStream = fs.createWriteStream(outputFilePath);
          // Download the video file
          ytdl.downloadFromInfo(info, { format: format }).pipe(outputStream);
          // When the download is complete, show a message
          outputStream.on('finish', () => {
             console.log(`Finished downloading: ${outputFilePath}`);
-         });
+            document.getElementById("converted").src = outputFilePath;
       });
 
    } catch (err) {
@@ -27,5 +32,6 @@ function validateAndSubmit()
       alert("Please Enter the Youtube Video URL");
       return;
    }
-   convertYoutubeToMp3(yout, document.getElementById("output").value);
+   document.getElementById("error").textContent = "asdfasdf";
+   convertYoutubeToMp3(yout, "test.mp3");
 }
