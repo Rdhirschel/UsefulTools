5892760fc9900c2c62f74ae645e79206b2b9f56e
creatorOfManyWonders <153096083+creatorOfManyWonders@users.noreply.github.com>
2023-12-17 12:05:27 +0000

    praying

---
+++ api/speak.js
@@ -0,0 +1,51 @@
+const { exec } = require('child_process');
+const fs = require('fs').promises;
+const { MongoClient } = require('mongodb');
+
+let mongoClient;
+const password = process.env.MONGODB_PASSWORD;
+async function connectToDatabase() 
+{
+  if (mongoClient) 
+  {
+    return mongoClient;
+  } 
+  
+  else 
+  {
+    mongoClient = new MongoClient(`mongodb+srv://rdhirschel:${password}@cluster0.gt6bpjp.mongodb.net/?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoClient.connect();
+    return mongoClient;
+  }
+}
+
+module.exports = async (req, res) => {
+    let {text} = req.query.body;
+
+    try {
+        await mongoClient.connect();
+        const db = mongoClient.db('textToSpeech');
+        const collection = db.collection('audioFiles');
+
+        // Check if the audio file already exists
+        const existingFile = await collection.findOne({ text: text });
+        if (existingFile) {
+            return res.download(existingFile.path);
+        }
+
+        const path = `audioFiles/${text.replace(/\s+/g, '_')}.mp3`;
+        exec(`espeak -w ${path} "${text}"`, (error) => {
+            if (error) {
+                console.error('Error:', error);
+                return res.status(500).json({ error: 'Server error' });
+            }
+            // Save the audio file in the database
+            collection.insertOne({ text: text, path: path });
+            res.download(path);
+
+        });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Server error' });
+    }
+};
