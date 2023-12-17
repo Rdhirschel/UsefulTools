00d18367d078d2efa450dddeed713094ad2163ba
creatorOfManyWonders <153096083+creatorOfManyWonders@users.noreply.github.com>
2023-12-17 07:48:58 +0000

    test please

--- scripts/y2mp3.js
+++ scripts/y2mp3.js
@@ -10,18 +10,21 @@ var fs = require(['fs'], function (fs) {
 
 async function convertYoutubeToMp3(url, outputFilename) {
    try {
-      const info = await ytdl.getInfo(url);
-      // Select the video format and quality
-      const format = ytdl.chooseFormat(info.formats, { quality: "248" });
-      // Create a write stream to save the video file
-      const outputFilePath = `${info.videoDetails.title}.${format.container}`;
-      const outputStream = fs.createWriteStream(outputFilePath);
-      // Download the video file
-      ytdl.downloadFromInfo(info, { format: format }).pipe(outputStream);
-      // When the download is complete, show a message
-      outputStream.on('finish', () => {
-         console.log(`Finished downloading: ${outputFilePath}`);
+      require(['ytdl-core', 'fluent-ffmpeg', 'fs'], function (ytdl, ffmpeg, fs) {
+         const info = ytdl.getInfo(url);
+         // Select the video format and quality
+         const format = ytdl.chooseFormat(info.formats, { quality: "248" });
+         // Create a write stream to save the video file
+         const outputFilePath = `${info.videoDetails.title}.${format.container}`;
+         const outputStream = fs.createWriteStream(outputFilePath);
+         // Download the video file
+         ytdl.downloadFromInfo(info, { format: format }).pipe(outputStream);
+         // When the download is complete, show a message
+         outputStream.on('finish', () => {
+            console.log(`Finished downloading: ${outputFilePath}`);
+         });
       });
+
    } catch (err) {
       console.error(err);
    }
