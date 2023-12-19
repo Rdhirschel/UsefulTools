b5803857ec24b4b67f523275eae9c2678bc58f64
Rdhirschel <hyrsldwrwn@gmail.com>
2023-12-19 10:46:47 +0200

    sasdasdsagdfsadasbgdy'poiioidghjiuytghjklio876tghjuy5trgfv65tgfb67ythg11 WORK PLEASE

--- api/mp3.js
+++ api/mp3.js
@@ -1,17 +1,20 @@
 const ytdl = require('ytdl-core'); 
 const ffmpeg = require('fluent-ffmpeg');
 const ffmpegPath = require('ffmpeg-static');
+const os = require('os');
+const path = require('path');
 
 ffmpeg.setFfmpegPath(ffmpegPath);
 
 module.exports = async (req, res) => {
     let { youtubeUrl } = req.body;
+    const tempDir = os.tmpdir();
 
     if (!youtubeUrl || !ytdl.validateURL(youtubeUrl)) {
       return res.status(400).json({ error: 'Invalid or missing YouTube URL in the request body' });
     }
 
-    let mp3Url = await convertYoutubeToMp3(youtubeUrl); 
+    let mp3Url = await convertYoutubeToMp3(youtubeUrl, tempDir); 
 
     return res.status(201).json({ message: 'MP3 URL created successfully', mp3Url: mp3Url });
   
@@ -21,7 +24,8 @@ module.exports = async (req, res) => {
 async function convertYoutubeToMp3(url) {
   return new Promise((resolve, reject) => {
     const stream = ytdl(url, { quality: 'highestaudio' });
-    const output = `./${Date.now()}.mp3`;
+    const tempDir = os.tmpdir();
+    const output = path.join(tempDir, `${Date.now()}.mp3`);
 
     ffmpeg(stream)
       .audioBitrate(128)
